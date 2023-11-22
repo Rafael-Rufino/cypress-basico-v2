e84b821b472cf3e37663b52c1dd37c2da7e4c096
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-11-22 17:23:57 -0300

    fix: fix case teste

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,7 +1,8 @@
 /// <reference types="Cypress" />
 
 describe('Central de atendimento ao Cliente TAT', () => {
-  it('Verifica o ttulo da aplicação', () => {
+  it('Verifica o titulo da aplicação', () => {
     cy.visit('./src/index.html')
+    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
   })
 })
